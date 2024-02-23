7008444183ff26b44cc404e14aa6e834a629a027
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-23 19:17:30 -0300

    Show a message if the cart is empty

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,17 +5,37 @@ import CartItem from './CartItem.jsx';
 export default function Cart ({ onCloseModal, onOpenCheckout }) {
   const { getCartTotal, getCartItems } = useContext(CartContext);
 
+  const total = getCartTotal();
+
   return (
     <div className="cart">
       <h2>Your cart</h2>
-      <ul>
-        {getCartItems().map(meal => <CartItem key={meal.id} id={meal.id} quantity={meal.quantity} name={meal.name} price={meal.price}/>)}
-      </ul>
-      <p className="cart-total">${getCartTotal()}</p>
-      <div className="modal-actions">
-        <button className="text-button" onClick={onCloseModal}>Close</button>
-        <button className="button" onClick={onOpenCheckout}>Go to Checkout</button>
-      </div>
+      {total === 0 && (
+        <>
+          <p>The cart is empty.</p>
+          <div className="modal-actions">
+            <button className="text-button" onClick={onCloseModal}>Close</button>
+          </div>
+        </>
+      )}
+
+      {total > 0 && (
+        <>
+          <ul>
+            {getCartItems().map(meal => <CartItem
+              key={meal.id}
+              id={meal.id}
+              quantity={meal.quantity}
+              name={meal.name}
+              price={meal.price}/>)}
+          </ul>
+          <p className="cart-total">${getCartTotal()}</p>
+          <div className="modal-actions">
+            <button className="text-button" onClick={onCloseModal}>Close</button>
+            <button className="button" onClick={onOpenCheckout}>Go to Checkout</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
