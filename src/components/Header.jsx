6128bed1542186e2ed8551e5075a0a5afc364c0d
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-22 23:17:05 -0300

    Update the quantity of cart button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,22 @@
 import imgLogo from '../assets/logo.jpg';
+import { useContext, useEffect, useState } from 'react';
+import { CartContext } from '../store/cart-context.jsx';
 
-export default function Header () {
+export default function Header ({ onOpenCart }) {
+  const { getCartQuantity } = useContext(CartContext);
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {
+    setQuantity(getCartQuantity());
+  }, [getCartQuantity]);
+
   return (
     <header id="main-header">
       <div id="title">
         <img src={imgLogo} alt="Food logo"/>
         <h1>React food</h1>
       </div>
-      <button>Cart (0)</button>
+      <button className='text-button' onClick={onOpenCart}>Cart ({quantity})</button>
     </header>
   );
 }
