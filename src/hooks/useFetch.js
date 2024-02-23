e8ff2556ca9eebf76374184b2f35b1c83d2bcb3c
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-23 13:28:11 -0300

    Create hook to manage fetch states and values

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export default function useFetch (initialValue, fetchFunction) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState({ message: '' });
+  const [fetchData, setFetchData] = useState(initialValue);
+
+  useEffect(() => {
+    (async () => {
+      setIsFetching(true);
+      try {
+        const data = await fetchFunction();
+        setFetchData(data);
+      } catch (e) {
+        setError({ message: e.message || 'Failed to fetch' });
+      }
+      setIsFetching(false);
+    })();
+  }, [fetchFunction]);
+
+  return {
+    isFetching,
+    error,
+    fetchData,
+    setFetchData
+  };
+}
