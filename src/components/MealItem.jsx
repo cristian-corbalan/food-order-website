e1b37e577a1ae50485d6a1d250296ef9dd84eec4
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-22 22:08:33 -0300

    Create MealItem component

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { CartContext } from '../store/cart-context.jsx';
+
+export default function MealItem ({ meal }) {
+  const { addItemToCart } = useContext(CartContext);
+
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={'http://localhost:3000/' + meal.image} alt={meal.name}/>
+        <div className="meal-item-description">
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">{meal.price}</p>
+          <p>{meal.description}</p>
+          <button className="button meal-item-actions" onClick={() => addItemToCart(meal.id)}>Add to Cart</button>
+        </div>
+      </article>
+    </li>
+  );
+}
