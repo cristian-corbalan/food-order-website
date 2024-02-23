1fc28eafca3758869b71fc92638b4ee4ad9398b0
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-23 16:19:18 -0300

    Create summary component

---
+++ src/components/Summary.jsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { CartContext } from '../store/cart-context.jsx';
+
+export default function Summary ({ onCloseModal }) {
+  const { getCartItems, getCartTotal } = useContext(CartContext);
+
+  return (
+    <div className="summary">
+      <h2>You order was sent!!</h2>
+      <h3>Summary</h3>
+      <ul>
+        {getCartItems().map(item => <li key={item.id}>{item.name} - {item.quantity} x {item.price}</li>)}
+      </ul>
+      <p className="summary-total">Total amount: ${getCartTotal()}</p>
+      <div className="modal-actions">
+        <button className="button" onClick={onCloseModal}>Confirm</button>
+      </div>
+    </div>
+  );
+}
