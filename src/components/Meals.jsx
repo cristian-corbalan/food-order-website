e751a9b0cd7b6d101f93bd1c5ec54758c4125cf8
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-23 13:28:55 -0300

    Add loader to Meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,14 +1,18 @@
 import MealItem from './MealItem.jsx';
+import Loader from './Loader.jsx';
 
-export default function Meals ({ meals }) {
+export default function Meals ({ meals, isLoading }) {
   return (
     <main>
       <h2>Our meals</h2>
-      <ul id="meals">
-        {meals.map(meal =>
-          <MealItem key={meal.id} meal={meal}/>
-        )}
-      </ul>
+      {isLoading && <Loader/>}
+      {!isLoading && (
+        <ul id="meals">
+          {meals.map(meal =>
+            <MealItem key={meal.id} meal={meal}/>
+          )}
+        </ul>
+      )}
     </main>
   );
 }
