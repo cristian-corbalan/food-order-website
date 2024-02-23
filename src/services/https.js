103da31f9c9fa9917f5f9172ce0878013bace59a
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-23 12:28:21 -0300

    Create function to send orders

--- src/services/https.js
+++ src/services/https.js
@@ -2,3 +2,16 @@ export async function fetchMeals () {
   const response = await fetch('http://localhost:3000/meals');
   return await response.json();
 }
+
+export async function sendOrder (order) {
+  const response = await fetch('http://localhost:3000/orders', {
+    method: 'POST',
+    body: JSON.stringify({
+      order
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return await response.json();
+}
