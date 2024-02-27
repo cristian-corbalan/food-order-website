eb846d29a21c7af4c99311aa3f46458a2f6b6201
cristian-corbalan <ccorbalandw@gmail.com>
2024-02-27 17:55:35 -0300

    Move meals fetch into the Meals component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,12 @@ import Meals from './components/Meals.jsx';
 import CartContextProvider from './store/cart-context.jsx';
 import Cart from './components/Cart.jsx';
 import Checkout from './components/Checkout.jsx';
-import useHttp from './hooks/useHttp.js';
-import Error from './components/Error.jsx';
 import Summary from './components/Summary.jsx';
 import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
 
-const httpConfig = {};
-
 function App () {
-  const { data: meals, error, isLoading } = useHttp('http://localhost:3000/meals', httpConfig, []);
-
   return (
-    <CartContextProvider meals={meals}>
+    <CartContextProvider>
       <UserProgressContextProvider>
         <Cart/>
 
@@ -24,8 +18,7 @@ function App () {
 
         <Header/>
 
-        {error.message && <Error title="Ups, something was wrong" message="Could not fetch our meals, try again later" />}
-        {!error.message && <Meals meals={meals} isLoading={isLoading}/>}
+        <Meals/>
       </UserProgressContextProvider>
     </CartContextProvider>
   );
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,7 +1,16 @@
 import MealItem from './MealItem.jsx';
 import Loader from './Loader.jsx';
+import useHttp from '../hooks/useHttp.js';
+import Error from './Error.jsx';
 
-export default function Meals ({ meals, isLoading }) {
+const httpConfig = {};
+export default function Meals () {
+  const { data: meals, error, isLoading } = useHttp('http://localhost:3000/meals', httpConfig, []);
+
+  if (error) {
+    return <Error title="Failed to fetch mels" message={error} />;
+  }
+
   return (
     <main>
       <h2 className="sr-only">Our meals</h2>
